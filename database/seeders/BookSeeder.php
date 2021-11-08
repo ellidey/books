96a8ebfa3d8592d332d129773d1c3bc7384ea744
ellidey <ellidey00@gmail.com>
2021-11-08 21:20:56 +0700

    Add book seeder

---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $faker = Factory::create('ru_RU');
+            DB::table('books')->insert([
+                'name' => $faker->word,
+                'description' => $faker->realText(1500),
+            ]);
+        }
+    }
+}
